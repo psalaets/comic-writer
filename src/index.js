33999b242436c95c277b783732a9f8e86637ef14
Paul Salaets <psalaets@gmail.com>
2018-09-21 18:46:54 -0400

    basic store set up

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import store from './store';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
