6bb0d0395af29815ec4103f362dd649c1b1fcb85
Cameron Leslie <cameronleslie@gmail.com>
2018-10-26 15:07:46 -0700

    Modal generalized.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 import ConnectedWriter from './components/writer/ConnectedWriter';
 import StatusLine from './components/status-line/StatusLine';
 import Button from './components/button/Button';
-import ModalBody from './components/modal-body/ModalBody';
-import Modal from 'react-aria-modal';
+import Modal from './components/modal/Modal';
 
 class App extends Component {
   constructor(props) {
@@ -17,39 +16,18 @@ class App extends Component {
 
     this.activateModal = this.activateModal.bind(this);
     this.deactivateModal = this.deactivateModal.bind(this);
-    this.getApplicationNode = this.getApplicationNode.bind(this);
   }
 
   activateModal = () => {
-    this.setState({ formattingModalActive: true });
+    this.setState({ modalActive: true });
   };
 
   deactivateModal = () => {
-    this.setState({ formattingModalActive: false });
+    this.setState({ modalActive: false });
   };
 
-  getApplicationNode = () => {
-    return document.getElementById('app');
-  };
 
   render() {
-    const formattingModal = this.state.formattingModalActive ? <Modal
-      titleText="Formatting Guide"
-      onExit={this.deactivateModal}
-      initialFocus="#close-formatting-modal"
-      getApplicationNode={this.getApplicationNode}
-      dialogClass="c-modal__dialouge"
-      underlayClass="c-modal"
-    >
-      <ModalBody
-        title="Formatting Guide"
-        closeButtonOnClick={this.deactivateModal}
-        closeButtonId="close-formatting-modal"
-      >
-        
-      </ModalBody>
-    </Modal> : false;
-
     return (
       <>
         <div className="c-app">
@@ -62,7 +40,13 @@ class App extends Component {
             </StatusLine>
           </div>
         </div>
-        {formattingModal}
+        <Modal
+          modalActive={this.state.modalActive}
+          closeButtonOnClick={this.deactivateModal}
+          title="Formatting Documentation"
+        >
+          Documentation Content here.
+        </Modal>
       </>
     );
   }
