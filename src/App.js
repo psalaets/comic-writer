795ada26fa285ef9585830874ce991b5ae2995a3
Cameron Leslie <cameronleslie@gmail.com>
2018-10-26 11:58:08 -0700

    Getting modal and statusline setup.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,60 @@ import React, { Component } from 'react';
 import './App.css';
 
 import ConnectedWriter from './components/writer/ConnectedWriter';
+import StatusLine from './components/status-line/StatusLine';
+import Button from './components/button/Button';
+import Modal from 'react-aria-modal';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      formattingModalActive: false
+    };
+
+    this.activateModal = this.activateModal.bind(this);
+    this.deactivateModal = this.deactivateModal.bind(this);
+    this.getApplicationNode = this.getApplicationNode.bind(this);
+  }
+
+  activateModal = () => {
+    this.setState({ formattingModalActive: true });
+  };
+
+  deactivateModal = () => {
+    this.setState({ formattingModalActive: false });
+  };
+
+  getApplicationNode = () => {
+    return document.getElementById('app');
+  };
+
   render() {
+    const formattingModal = this.state.formattingModalActive ? <Modal
+      titleText="Formatting Guide"
+      onExit={this.deactivateModal}
+      initialFocus="#close-formatting-modal"
+      getApplicationNode={this.getApplicationNode}
+    >
+      <h2 id="formatting-guide">Formatting Guide</h2>
+      <Button id="close-formatting-modal" onClick={this.deactivateModal}>Close X</Button>
+    </Modal> : false;
+
     return (
-      <div className="c-app">
-        <ConnectedWriter />
-      </div>
+      <>
+        <div className="c-app">
+          <div className="c-app__writer">
+            <ConnectedWriter />
+          </div>
+          <div className="c-app__status-line">
+            <StatusLine>
+              <Button onClick={this.activateModal}>Formatting Help</Button>
+            </StatusLine>
+          </div>
+        </div>
+        {formattingModal}
+      </>
     );
   }
 }
