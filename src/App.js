7a2772fcd5424ebba3b8407b81a1850e7c0d9f11
Cameron Leslie <cameronleslie@gmail.com>
2018-10-28 09:51:49 -0700

    Whoops.

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     super(props);
 
     this.state = {
-      modalActive: true
+      modalActive: false
     };
 
     this.activateModal = this.activateModal.bind(this);
