18412e905496d9d743d6b7e80109e45cba6a492f
Cameron Leslie <cameronleslie@gmail.com>
2018-10-28 10:35:19 -0700

    Getting it started. No live data for all values.

--- src/components/page/Page.js
+++ src/components/page/Page.js
@@ -8,21 +8,14 @@ export default class Page extends Component {
       <section className="c-page">
         <h2 className="c-page__title">
           <span className="u-font-size--marcus">Page {this.props.number}</span>
-          <span className="u-hide--visually">: </span>
-          <span className=" u-font-size--saya c-page__panel-count">{this.panelCount()}</span>
+          <span className=" u-font-size--saya c-page__panel-count">
+            <span className="u-hide--visually">Panel Count: </span>
+            {this.props.panelCount}
+          </span>
         </h2>
         {this.props.children}
       </section>
     );
-  }
-
-  panelCount() {
-    if (this.props.panelCount === 0) {
-      return '';
-    }
-
-    const label = this.props.panelCount === 1 ? 'Panel' : 'Panels';
-    return `${this.props.panelCount} ${label}`;
   }
 }
 
--- src/components/panel/Panel.js
+++ src/components/panel/Panel.js
@@ -4,9 +4,16 @@ import './Panel.css';
 
 export default class Panel extends Component {
   render() {
+    console.log(this.props);
     return (
       <section className="c-panel">
-        <h3 className="c-panel__title u-font-size--maria">Panel {this.props.number}</h3>
+        <h3 className="c-panel__title">
+          <span className="u-font-size--maria"> Panel {this.props.number}</span>
+          <span className=" u-font-size--saya c-panel__dialouge-count">
+            <span className="u-hide--visually">Dialoug Count: </span>
+            10
+          </span>
+        </h3>
         {this.props.children}
       </section>
     );
