e644866affc6d49c69c1d2d5e480f001f9b1b5f9
Cameron Leslie <cameronleslie@gmail.com>
2018-10-29 13:21:12 -0700

    Helping things make more sense.

--- src/components/formatting-guide/FormattingGuide.js
+++ src/components/formatting-guide/FormattingGuide.js
@@ -3,17 +3,23 @@ import './FormattingGuide.css';
 
 import FormattingGuideData from '../../documentation/FormattingGuideData'
 
-const GuideDataToJSX = data => data.map(a => [
-  <dt className="c-formatting-guide__title u-font-size--saya">{a.title}</dt>,
-  <dd className="c-formatting-guide__description">
+const GuideDataToJSX = data => data.map((a, i) => [
+  <dt key={i} className="c-formatting-guide__title u-font-size--saya">{a.title}</dt>,
+  <dd key={i + 1} className="c-formatting-guide__description">
     <pre className="c-formatting-guide__code">{a.code}</pre>
   </dd>
 ]);
 
 const FormattingGuide = props => (
-  <dl className="c-formatting-guide">
-    {GuideDataToJSX(FormattingGuideData)}
-  </dl>
+  <>
+    Getting Started
+    <hr/>
+    <dl className="c-formatting-guide">
+      {GuideDataToJSX(FormattingGuideData)}
+    </dl>
+    <hr/>
+    <a href="https://gitlab.com/psalaets/comic-writer/blob/master/src/documentation/readme.md" rel="noopener noreferrer" target="_blank">Need More Help?</a>
+  </>
 );
 
 export default FormattingGuide;
