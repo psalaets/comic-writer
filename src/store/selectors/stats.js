d27f8fc5d960d438004769f5b5220317f0bf6ca7
Paul Salaets <psalaets@gmail.com>
2018-12-08 14:52:46 -0500

    compute stats in a memoized selector

---
+++ src/store/selectors/stats.js
@@ -0,0 +1,54 @@
+import { createSelector } from 'reselect';
+
+import parse from '../../parser';
+import visit from '../../parser/visit';
+import * as types from '../../types';
+
+export const statsSelector = createSelector(
+  state => state.editor.source,
+  source => parseStats(source)
+);
+
+function parseStats(source) {
+  const stats = [];
+  let panelsSeen = 0;
+
+  visit(parse(source), {
+    enterPage(page) {
+      panelsSeen = 0;
+    },
+    exitPage(page) {
+      stats.push({
+        type: types.PAGE,
+        lineNumber: page.startingLine,
+        wordCount: page.dialogueWordCount + page.captionWordCount,
+        panelCount: panelsSeen
+      });
+    },
+    enterPanel(panel) {
+      panelsSeen += 1;
+
+      stats.push({
+        type: types.PANEL,
+        lineNumber: panel.startingLine,
+        wordCount: panel.dialogueWordCount + panel.captionWordCount
+      });
+    },
+    enterDialogue(dialogue) {
+      stats.push({
+        type: types.DIALOGUE,
+        lineNumber: dialogue.startingLine,
+        wordCount: dialogue.wordCount
+      });
+    },
+    enterCaption(caption) {
+      stats.push({
+        type: types.CAPTION,
+        lineNumber: caption.startingLine,
+        wordCount: caption.wordCount
+      });
+    }
+  });
+
+  return stats;
+}
