714431034d38b2b49bc448b0fa419161cdeca9a8
Paul Salaets <psalaets@gmail.com>
2018-12-08 16:26:33 -0500

    bulk editor changes should be done within an operation

--- src/codemirror/panel-counts/index.js
+++ src/codemirror/panel-counts/index.js
@@ -1,18 +1,18 @@
 import { PAGE } from '../../types';
 
 export function create(cm) {
+  let widgets = [];
+
   return {
     update(stats) {
-      cm.getDoc().eachLine(lineHandle => {
-        const lineInfo = cm.getDoc().lineInfo(lineHandle);
-        if (lineInfo.widgets && lineInfo.widgets.length > 0) {
-          lineInfo.widgets.forEach(widget => widget.clear());
-        }
-      });
+      cm.operation(() => {
+        // clear existing widgets
+        widgets.forEach(widget => widget.clear());
 
-      const pages = stats.filter(tuple => tuple.type === PAGE);
-      pages.forEach(page => {
-        cm.getDoc().addLineWidget(page.lineNumber - 1, node(page.panelCount));
+        // add new widgets
+        widgets = stats
+          .filter(tuple => tuple.type === PAGE)
+          .map(page => cm.addLineWidget(page.lineNumber - 1, node(page.panelCount)));
       });
 
       cm.refresh();
