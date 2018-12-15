2deb8414e907aa2bff765602b4f4c9c2d212c5a8
Paul Salaets <psalaets@gmail.com>
2018-12-14 23:13:26 -0500

    these aren't token ids, they become css classes

--- src/codemirror/comic-writer-mode/index.js
+++ src/codemirror/comic-writer-mode/index.js
@@ -4,13 +4,13 @@ export const MODE = 'comic-writer';
 export const THEME = 'comic-writer-light';
 
 // these values become css classes so keep them synced with theme file
-const PAGE_TOKEN = 'page';
-const PANEL_TOKEN = 'panel';
-const SFX_TOKEN = 'sfx';
-const CAPTION_TOKEN = 'caption';
-const DIALOGUE_TOKEN = 'dialogue';
-const LETTERING_BOLD_TOKEN = 'lettering-bold';
-const METADATA_TOKEN = 'metadata';
+const PAGE_CLASS = 'page';
+const PANEL_CLASS = 'panel';
+const SFX_CLASS = 'sfx';
+const CAPTION_CLASS = 'caption';
+const DIALOGUE_CLASS = 'dialogue';
+const LETTERING_BOLD_CLASS = 'lettering-bold';
+const METADATA_CLASS = 'metadata';
 
 CodeMirror.defineMode(MODE, cmConfig => {
   return {
@@ -22,33 +22,33 @@ CodeMirror.defineMode(MODE, cmConfig => {
     },
     indent: () => 0,
     token(stream, state) {
-      if (stream.match(/^\tsfx ?(\(.+\))?: ?(.+)$/i)) return SFX_TOKEN;
+      if (stream.match(/^\tsfx ?(\(.+\))?: ?(.+)$/i)) return SFX_CLASS;
 
       // match the first part of a caption
       if (stream.match(/^\tcaption ?(\(.+\))?: ?/i)) {
         state.isInCaptionText = !stream.eol();
-        return CAPTION_TOKEN;
+        return CAPTION_CLASS;
       }
 
       // handle caption text
       if (state.isInCaptionText) {
-        return tokenLetteringText(stream, state, 'isInCaptionText', CAPTION_TOKEN);
+        return tokenLetteringText(stream, state, 'isInCaptionText', CAPTION_CLASS);
       }
 
       // dialogue matching must be after sfx and caption to prevent characters
       // named "sfx" and "caption"
       if (stream.match(/^\t(.+) ?(\(.+\))?: ?/)) {
         state.isInDialogueText = !stream.eol();
-        return DIALOGUE_TOKEN;
+        return DIALOGUE_CLASS;
       }
 
       if (state.isInDialogueText) {
-        return tokenLetteringText(stream, state, 'isInDialogueText', DIALOGUE_TOKEN);
+        return tokenLetteringText(stream, state, 'isInDialogueText', DIALOGUE_CLASS);
       }
 
-      if (stream.match(/^page \d+$/i)) return PAGE_TOKEN;
-      if (stream.match(/^panel \d+$/i)) return PANEL_TOKEN;
-      if (stream.match(/^(.+): ?(.+)/)) return METADATA_TOKEN;
+      if (stream.match(/^page \d+$/i)) return PAGE_CLASS;
+      if (stream.match(/^panel \d+$/i)) return PANEL_CLASS;
+      if (stream.match(/^(.+): ?(.+)/)) return METADATA_CLASS;
 
       // advance stream past stuff that isn't styled, like plain paragraphs
       stream.skipToEnd();
@@ -75,7 +75,7 @@ function tokenLetteringText(stream, state, stateFlagName, defaultToken) {
     // and there is another double star somewhere
     if (stream.match(/.*?\*\*/)) {
       // that was a run of lettering-bold
-      tokens.push(LETTERING_BOLD_TOKEN);
+      tokens.push(LETTERING_BOLD_CLASS);
     }
     // stream is at an unpaired double star
     else {
