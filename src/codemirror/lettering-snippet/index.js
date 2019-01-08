6d542809b9186395e44e4aefb80b5e3c1c359e56
Paul Salaets <psalaets@gmail.com>
2019-01-07 22:45:57 -0500

    messy first impl of auto complete

--- src/codemirror/lettering-snippet/index.js
+++ src/codemirror/lettering-snippet/index.js
@@ -1,9 +1,9 @@
 import CodeMirror from 'codemirror';
 
-export function letteringSnippet(cm) {
+export function letteringSnippet(cm, getCharacterNames) {
   const lineNumber = cm.getCursor().line;
   let stepIndex = -1;
-  const steps = makeSteps();
+  const steps = makeSteps(getCharacterNames);
 
   const keyMap = {
     Tab() {
@@ -64,22 +64,69 @@ export function letteringSnippet(cm) {
   }
 }
 
-function makeSteps() {
+function makeSteps(getCharacterNames) {
   return [
     function metadataState(cm) {
+      const suggestionsSnapshot = ['sfx', 'caption'].concat(getCharacterNames());
       const cursor = cm.getCursor();
       const lineText = cm.getLine(cursor.line);
       const tabIndex = lineText.indexOf('\t');
       const colonIndex = lineText.indexOf(':');
 
-      cm.setSelection({
-        line: cursor.line,
-        ch: tabIndex + 1
-      },
+      cm.setSelection(
+        {
+          line: cursor.line,
+          ch: tabIndex + 1
+        },
         {
           line: cursor.line,
           ch: colonIndex
-        });
+        }
+      );
+
+
+      function hint() {
+        const suggestions = suggestionsSnapshot
+          .filter(name => {
+            const cursor = cm.getCursor();
+            const token = cm.getTokenAt(cursor);
+
+            return token.string === 'subject' || name.startsWith(token.string);
+          })
+          .map(name => {
+            return {
+              text: name,
+              displayText: name.toUpperCase(),
+              hint(cm) {
+                const cursor = cm.getCursor();
+                const token = cm.getTokenAt(cursor);
+
+                const from = {
+                  line: cursor.line,
+                  ch: token.start
+                };
+
+                const to = {
+                  line: cursor.line,
+                  ch: token.end
+                };
+
+                cm.replaceRange(name, from, to);
+              }
+            };
+          });
+
+        return {
+          list: suggestions
+        };
+      }
+
+      hint.supportsSelection = true;
+
+      cm.showHint({
+        hint,
+        completeSingle: false,
+      });
     },
     function contentState(cm) {
       const cursor = cm.getCursor();
