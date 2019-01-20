5cc55784235b30a9d8f5155828f12b89fe40aea2
Paul Salaets <psalaets@gmail.com>
2019-01-20 14:18:35 -0500

    really basic 'bold what is selected' support

--- src/codemirror/lettering-bold-command/index.js
+++ src/codemirror/lettering-bold-command/index.js
@@ -1,8 +1,16 @@
 export function letteringBoldCommand(cm) {
   const cursor = cm.getCursor();
 
-  if (!cm.somethingSelected()) {
-    const cursor = cm.getCursor();
+  if (cm.somethingSelected()) {
+    const selections = cm.listSelections();
+
+    if (selections.length > 0) {
+      const selection = selections[0];
+      const selectedText = cm.getRange(selection.anchor, selection.head);
+
+      cm.replaceRange(`**${selectedText}**`, selection.anchor, selection.head);
+    }
+  } else {
     cm.replaceRange('****', {
       line: cursor.line,
       ch: cursor.ch
