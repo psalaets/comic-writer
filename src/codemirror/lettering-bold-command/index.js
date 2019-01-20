b965a58297afa860bcb90bec64f3192c9712c738
Paul Salaets <psalaets@gmail.com>
2019-01-20 15:07:26 -0500

    work no matter if selection was left to right or vice versa

--- src/codemirror/lettering-bold-command/index.js
+++ src/codemirror/lettering-bold-command/index.js
@@ -5,10 +5,10 @@ export function letteringBoldCommand(cm) {
     const selections = cm.listSelections();
 
     if (selections.length > 0) {
-      const selection = selections[0];
-      const selectedText = cm.getRange(selection.anchor, selection.head);
+      const selection = orderLeftToRight(selections[0]);
+      const selectedText = cm.getRange(...selection);
 
-      cm.replaceRange(`**${selectedText}**`, selection.anchor, selection.head);
+      cm.replaceRange(`**${selectedText}**`, ...selection);
     }
   } else {
     cm.replaceRange('****', {
@@ -20,4 +20,25 @@ export function letteringBoldCommand(cm) {
       ch: cursor.ch + 2
     });
   }
+}
+
+function orderLeftToRight(selection) {
+  const {anchor, head} = selection;
+
+  // on different lines
+  if (anchor.line < head.line) {
+    return [anchor, head];
+  } else if (head.line < anchor.line) {
+    return [head, anchor];
+  }
+
+  // on same line
+  if (anchor.ch < head.ch) {
+    return [anchor, head];
+  } else if (head.ch < anchor.ch) {
+    return [head, anchor];
+  }
+
+  // at same exact spot
+  return [anchor, head];
 }
