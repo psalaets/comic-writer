4d4af9da9ce8df388b13b53c254f81e68405848e
Paul Salaets <psalaets@gmail.com>
2019-12-07 00:16:07 -0500

    update tests for rename

--- src/store/reducers/editor/index.test.js
+++ src/store/reducers/editor/index.test.js
@@ -1,12 +1,12 @@
-import { transformMarkdown } from './index';
+import { preprocessSource } from './index';
 
 describe('editor reducer', () => {
-  describe('transformMarkdown', () => {
+  describe('preprocessSource', () => {
     describe('single page', () => {
       it('first page', () => {
         const input = `page`;
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -14,7 +14,7 @@ describe('editor reducer', () => {
       it('adding page at start', () => {
         const input = 'page\nPage 1\nthis is page 1 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -22,7 +22,7 @@ describe('editor reducer', () => {
       it('adding page at end', () => {
         const input = 'Page 1\nthis is page 1 info\npage';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -30,7 +30,7 @@ describe('editor reducer', () => {
       it('adding page in the middle', () => {
         const input = 'Page 1\nthis is page 1 info\npage\nPage 2\nthis is page 2 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -38,7 +38,7 @@ describe('editor reducer', () => {
       it('inserting page at start', () => {
         const input = 'Page 3\nthis is page 3 info\nPage 1\nthis is page 1 info\nPage 2\nthis is page 2 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -46,7 +46,7 @@ describe('editor reducer', () => {
       it('inserting page at end', () => {
         const input = 'Page 2\nthis is page 2 info\nPage 1\nthis is page 1 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -54,7 +54,7 @@ describe('editor reducer', () => {
       it('inserting page in middle', () => {
         const input = 'Page 1\nthis is page 1 info\nPage 3\nthis is page 3 info\nPage 2\nthis is page 2 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -62,7 +62,7 @@ describe('editor reducer', () => {
       it('remove page from start', () => {
         const input = 'Page 2\nthis is page 2 info\nPage 3\nthis is page 3 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -70,7 +70,7 @@ describe('editor reducer', () => {
       it('remove page from middle', () => {
         const input = 'Page 1\nthis is page 1 info\nPage 3\nthis is page 3 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -79,7 +79,7 @@ describe('editor reducer', () => {
         const tenPages = Array.from(new Array(10), (p, i) => `Page ${i + 1}`).join('\n');
         const input = tenPages + '\npage\npage\npage';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -89,7 +89,7 @@ describe('editor reducer', () => {
       it('adding panel to page', () => {
         const input = 'Page 1\nthis is page 1 info\npanel';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -97,7 +97,7 @@ describe('editor reducer', () => {
       it('adding second panel to page', () => {
         const input = 'Page 1\nthis is page 1 info\nPanel 1\npanel';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -105,7 +105,7 @@ describe('editor reducer', () => {
       it('adding panel at start', () => {
         const input = 'Page 1\nthis is page 1 info\npanel\nPanel 1\nthis is panel 1 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -113,7 +113,7 @@ describe('editor reducer', () => {
       it('adding panel in middle', () => {
         const input = 'Page 1\nPanel 1\nthis is panel 1 info\npanel\nPanel 2\nthis is panel 2 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -121,7 +121,7 @@ describe('editor reducer', () => {
       it('adding panel to second page', () => {
         const input = 'Page 1\nPanel 1\nthis is panel 1 info\nPage 2\npanel';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -129,7 +129,7 @@ describe('editor reducer', () => {
       it('adding another panel to second page', () => {
         const input = 'Page 1\nPanel 1\nthis is panel 1 info\nPage 2\nPanel 1\npanel';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -137,7 +137,7 @@ describe('editor reducer', () => {
       it('inserting panel at start', () => {
         const input = 'Page 1\nthis is page 1 info\nPanel 2\nPanel 1\nthis is panel 1 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -145,7 +145,7 @@ describe('editor reducer', () => {
       it('inserting panel in middle', () => {
         const input = 'Page 1\nPanel 1\nthis is panel 1 info\nPanel 3\nPanel 2\nthis is panel 2 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -153,7 +153,7 @@ describe('editor reducer', () => {
       it('removing panel from start', () => {
         const input = 'Page 1\nPanel 2\nthis is panel 2 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
@@ -161,7 +161,7 @@ describe('editor reducer', () => {
       it('removing panel from middle', () => {
         const input = 'Page 1\nPanel 1\nthis is panel 1 info\nPanel 3\nthis is panel 3 info';
 
-        const result = transformMarkdown(input);
+        const result = preprocessSource(input);
 
         expect(result).toMatchSnapshot();
       });
