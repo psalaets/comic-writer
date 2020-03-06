379057018f5df2a30725274a32bfdd6e8e3b7255
Paul Salaets <psalaets@gmail.com>
2020-03-05 19:48:29 -0500

    impl of new idea

--- src/editor/components/codemirror/gutters/word-counts.ts
+++ src/editor/components/codemirror/gutters/word-counts.ts
@@ -1,6 +1,11 @@
-import { Editor } from 'codemirror';
+import { Editor, LineHandle } from 'codemirror';
 import { WordCount } from '../../../../script/types';
 export const ID = 'word-counts';
+
+type HandleTuple = {
+  handle: LineHandle,
+  wordCount: WordCount
+};
 
 /**
  * Creates an object that updates the word counts gutter in the CodeMirror
@@ -14,25 +19,71 @@ export function create(cm: Editor) {
   };
 }
 
-// updates and removes
-//    for every cached line handle
-//      if its line number has something in the new and word count is different than cached
-//        update it and cache new line handle
-//      else if line number doesn't have anything in new
-//        remove it
-//      end
-//    end
-//
-// adds
-//    for every incoming word count
-//      if that line didn't already have a thing added
-//        add it and cache line handle
-
-
 function createUpdater(cm: Editor) {
+  /** Lines (w/metadata) that had word count gutters in the last pass */
+  let existingHandles: Array<HandleTuple> = [];
+
   return function updateWordCounts(wordCounts: Array<WordCount>, prevCounts: Array<WordCount>) {
     cm.operation(() => {
+      /** Lines (w/metadata) that have word count gutters in this pass */
+      const newHandles: Array<HandleTuple> = [];
+      /** One-based line numbers that have been updated on this pass */
+      const updatedLines = new Set<number>();
 
+      const wordCountsByLineNumber = wordCounts.reduce((byLine, wordCount) => {
+        byLine[wordCount.lineNumber] = wordCount;
+        return byLine;
+      }, {} as {[id: number]: WordCount});
+
+      // Look at all lines that had gutters on the last pass, and see if the
+      // gutter should be updated, left alone or removed.
+      existingHandles
+        .forEach(tuple => {
+          const lineInfo = cm.lineInfo(tuple.handle);
+
+          // Line was deleted, there's no gutter to care about
+          if (!lineInfo) return;
+
+          // Look up the line's word count for this pass, if any
+          const wordCount = wordCountsByLineNumber[lineInfo.line + 1];
+
+          if (shouldHaveGutter(wordCount)) {
+            // mark this line as: word count has been updated
+            updatedLines.add(wordCount.lineNumber);
+
+            // count changed so we actually need to update the gutter
+            if (wordCount.count !== tuple.wordCount.count) {
+              newHandles.push({
+                handle: cm.setGutterMarker(lineInfo.line, ID, element(wordCount)),
+                wordCount
+              });
+            } else {
+              // count didn't change, gutter should remain as-is
+              newHandles.push(tuple);
+            }
+          } else { // count should be removed
+            if (hasGutter(lineInfo)) {
+              cm.setGutterMarker(lineInfo.line, ID, null);
+            }
+          }
+        });
+
+      // Render newly added word counts
+      wordCounts
+        // ignore counts that don't need a gutter
+        .filter(wordCount => shouldHaveGutter(wordCount))
+        // ignore counts that were updated above
+        .filter(wordCount => !updatedLines.has(wordCount.lineNumber))
+        // show the count
+        .forEach(wordCount => {
+          newHandles.push({
+            handle: cm.setGutterMarker(wordCount.lineNumber - 1, ID, element(wordCount)),
+            wordCount
+          });
+        });
+
+      // all count on this pass will become the "old counts" for next pass
+      existingHandles = newHandles;
     });
   };
 }
@@ -43,39 +94,6 @@ function shouldHaveGutter(wordCount: WordCount): boolean {
 
 function hasGutter(lineInfo: any): boolean {
   return lineInfo.gutterMarkers != null && lineInfo.gutterMarkers[ID] != null;
-}
-
-/**
- * Tells if all nodes are in the same spot as last time. If this is true, we can
- * update word counts by finding node where their count is different.
- *
- * This happens when user is changing, but not deleting, an existing line of
- * dialogue/caption.
- *
- * @param wordCounts Current word counts
- * @param prevCounts Last time's word counts
- */
-function sameStructure(wordCounts: Array<WordCount>, prevCounts: Array<WordCount>): boolean {
-  const sameLength = wordCounts.length === prevCounts.length;
-  if (!sameLength) return false;
-
-  return wordCounts.every((count, index) => {
-    const prev = prevCounts[index];
-    return count.nodeId === prev.nodeId && count.lineNumber === prev.lineNumber;
-  });
-}
-
-/**
- * Assuming structure is the same, find nodes with different word count.
- *
- * @param wordCounts
- * @param prevCounts
- */
-function findDifferentCounts(wordCounts: Array<WordCount>, prevCounts: Array<WordCount>): Array<WordCount> {
-  return wordCounts.filter((count, index) => {
-    const prev = prevCounts[index];
-    return count.count !== prev.count;
-  });
 }
 
 function element(wordCount: WordCount) {
