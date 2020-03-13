dfd723d2dd175277a58bc890d05d2f8b1c9f56c5
Paul Salaets <psalaets@gmail.com>
2020-03-13 16:56:48 -0400

    measure preprocessing time

--- src/editor/components/codemirror/preprocessor/index.ts
+++ src/editor/components/codemirror/preprocessor/index.ts
@@ -1,3 +1,4 @@
+import * as perf from '../../../../perf';
 import classifyLines from './classify-lines';
 import autoNumber from './auto-number';
 
@@ -11,14 +12,13 @@ export function preprocessLines(
   lines: Array<string>,
   cursorLine: number
 ): Array<string> {
-  // performance.mark('start:preprocess-source');
+  perf.start('preprocess-source');
 
   const processedLines = lines
     .map(classifyLines(cursorLine))
     .map(autoNumber());
 
-  // performance.mark('end:preprocess-source');
-  // performance.measure('preprocess-source', 'start:preprocess-source', 'end:preprocess-source');
+  perf.end('preprocess-source');
 
   return processedLines;
 }
