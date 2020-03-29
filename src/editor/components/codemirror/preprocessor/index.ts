ff8bab2c59ef72b352956299e583058458c843bc
Paul Salaets <psalaets@gmail.com>
2020-03-29 15:58:23 -0400

    auto numbering always starts at 1 so we can't just start numbering somewhere in the middle, so only cache classifications

--- src/editor/components/codemirror/preprocessor/index.ts
+++ src/editor/components/codemirror/preprocessor/index.ts
@@ -1,28 +1,44 @@
-import { wrap } from '../../../../perf';
+import * as perf from '../../../../perf';
 import createClassifier from './classify';
 import autoNumber from './auto-number';
+import { LineClassification } from './types';
 
 /**
  * Performs auto-numbering and keyword expansion on the script source.
  *
  * @param lines Lines of the script
- * @param cursorLine What line the cursor is on
- * @param fromLine First line that could have a change
+ * @param cursorLine What line number the cursor is on
+ * @param fromLine Line number of first line in the script that has a change
  */
-export const preprocessLines = wrap('preprocessLines', preprocess);
+export const preprocessLines = perf.wrap('preprocessLines', preprocess);
+
+let lastClassifications: Array<LineClassification> = [];
 
 function preprocess(
   lines: Array<string>,
   cursorLine: number,
   fromLine: number
 ): Array<string> {
-  const needsProcessing = lines.slice(fromLine);
-  const lineOffset = fromLine;
 
-  const processedLines = needsProcessing
-    .map(createClassifier(cursorLine, lineOffset))
+  perf.start('classify-lines');
+
+  const unchangedClassifications = lastClassifications.slice(0, fromLine);
+  const changedLines = lines.slice(unchangedClassifications.length);
+
+  const changedClassifications = changedLines
+    .map(createClassifier(cursorLine, fromLine));
+
+  const allClassifications
+    = lastClassifications
+    = unchangedClassifications.concat(changedClassifications);
+
+  perf.end('classify-lines');
+  perf.start('number-lines');
+
+  const numberedLines = allClassifications
     .map(autoNumber());
 
-  const unchanged = lines.slice(0, fromLine);
-  return unchanged.concat(processedLines);
+  perf.end('number-lines');
+
+  return numberedLines;
 }
