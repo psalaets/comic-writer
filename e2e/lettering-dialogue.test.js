210a6b23b77e0545682497c240f8d68675c9843a
Paul Salaets <psalaets@gmail.com>
2020-05-03 14:17:12 -0400

    few more dialogue tests and found a bug with speaker capitalization

--- e2e/lettering-dialogue.test.js
+++ e2e/lettering-dialogue.test.js
@@ -50,3 +50,104 @@ test('with modifier for new speaker, only using keyboard', async t => {
     '    bob (OFF): this is the dialogue content'
   ]);
 });
+
+test('for existing speaker, only using keyboard', async t => {
+  await t
+    .typeText(selectors.editorContent(), 'page')
+    .pressKey('enter')
+    .typeText(selectors.editorContent(), 'panel')
+    .pressKey('enter')
+    // first balloon
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'bob')
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'first balloon')
+    .pressKey('enter')
+    // second balloon, select bob from popup
+    .pressKey('tab')
+    .pressKey('down')
+    .pressKey('down')
+    .pressKey('enter')
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'second balloon')
+
+  const lines = await editorLines();
+
+  await t.expect(lines).eql([
+    'Page 1',
+    'Panel 1',
+    '    bob: first balloon',
+    '    BOB: second balloon',
+  ]);
+});
+
+test('with modifier for existing speaker, only using keyboard', async t => {
+  await t
+    .typeText(selectors.editorContent(), 'page')
+    .pressKey('enter')
+    .typeText(selectors.editorContent(), 'panel')
+    .pressKey('enter')
+    // first balloon
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'bob')
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'first balloon')
+    .pressKey('enter')
+    // second balloon, select bob from popup
+    .pressKey('tab')
+    .pressKey('down')
+    .pressKey('down')
+    .pressKey('enter')
+    .typeText(selectors.editorContent(), ' ')
+    .pressKey('shift+9')
+    .typeText(selectors.editorContent(), 'OFF')
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'second balloon')
+
+  const lines = await editorLines();
+
+  await t.expect(lines).eql([
+    'Page 1',
+    'Panel 1',
+    '    bob: first balloon',
+    '    BOB (OFF): second balloon',
+  ]);
+});
+
+test('speakers in popup are in abc order', async t => {
+  await t
+    .typeText(selectors.editorContent(), 'page')
+    .pressKey('enter')
+    .typeText(selectors.editorContent(), 'panel')
+    .pressKey('enter')
+    // first balloon
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'zzz')
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'first balloon')
+    .pressKey('enter')
+    // second balloon
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'aaa')
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'second balloon')
+    .pressKey('enter')
+    // third balloon, need to arrow past aaa to select zzz from popup
+    .pressKey('tab')
+    .pressKey('down')
+    .pressKey('down')
+    .pressKey('down')
+    .pressKey('enter')
+    .pressKey('tab')
+    .typeText(selectors.editorContent(), 'third balloon')
+
+  const lines = await editorLines();
+
+  await t.expect(lines).eql([
+    'Page 1',
+    'Panel 1',
+    '    zzz: first balloon',
+    '    aaa: second balloon',
+    '    ZZZ: third balloon',
+  ]);
+});
