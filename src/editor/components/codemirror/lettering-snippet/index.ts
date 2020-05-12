20c8807eee9cdd2d5d649f48486a605ca88b124c
Paul Salaets <psalaets@gmail.com>
2020-05-11 22:01:26 -0400

    clean up in lettering snippet

--- src/editor/components/codemirror/lettering-snippet/index.ts
+++ src/editor/components/codemirror/lettering-snippet/index.ts
@@ -2,11 +2,6 @@ import * as CodeMirror from 'codemirror';
 import 'codemirror/addon/hint/show-hint';
 import 'codemirror/addon/hint/show-hint.css';
 
-import {
-  LETTERING_SUBJECT,
-  LETTERING_MODIFIER,
-  LETTERING_CONTENT
-} from '../mode/token';
 import { LetteringLine } from './lettering-line';
 
 const SUBJECT_PLACEHOLDER = 'SUBJECT';
@@ -16,6 +11,11 @@ const SUBJECT_PLACEHOLDER = 'SUBJECT';
  * Keep this synced with theme.css
  */
 const LETTERING_SNIPPET_CLASS = 'lettering-snippet';
+
+type TabStop = {
+  name: string,
+  activate: () => 'skip' | void;
+};
 
 /**
  * A code-editor style snippet for creating dialogue/caption/sfx.
@@ -28,10 +28,179 @@ export function letteringSnippet(
   getCharacterNames: () => Array<string>
 ) {
   const lineNumber = cm.getCursor().line;
-  let stepIndex = 0;
-  const steps = makeSteps();
 
-  const keyMap: CodeMirror.KeyMap = {
+  let tabStopIndex = 0;
+  const tabStops = makeTabStops(cm);
+  const keyMap = makeKeyMap(nextStop, previousStop, exitSnippet);
+
+  enterSnippet();
+  nextStop();
+  showHint();
+
+  function enterSnippet() {
+    cm.addKeyMap(keyMap);
+    cm.on('cursorActivity', handleCursorActivity);
+    cm.addLineClass(lineNumber, 'text', LETTERING_SNIPPET_CLASS);
+
+    cm.replaceRange(`\t${SUBJECT_PLACEHOLDER}: content`, cm.getCursor());
+  }
+
+  function exitSnippet() {
+    cm.removeLineClass(lineNumber, 'text', LETTERING_SNIPPET_CLASS);
+    cm.off('cursorActivity', handleCursorActivity);
+    cm.removeKeyMap(keyMap);
+  }
+
+  function previousStop() {
+    move(-1);
+  }
+
+  function nextStop() {
+    move(1);
+  }
+
+  function move(delta: number) {
+    tabStopIndex += delta;
+
+    const stop = tabStops[tabStopIndex];
+
+    if (stop) {
+      const result = stop.activate();
+
+      if (result === 'skip') {
+        move(delta);
+      }
+    } else {
+      exitSnippet();
+    }
+  }
+
+  // Exit if it seems like user is trying to get out of snippet
+  function handleCursorActivity(cm: CodeMirror.Editor) {
+    // moved to different line
+    if (lineNumber !== cm.getCursor().line) {
+      exitSnippet();
+    }
+
+    // moved to start of line
+    if (cm.getCursor().ch === 0) {
+      exitSnippet();
+    }
+  }
+
+  function showHint() {
+    cm.showHint({
+      hint: makeHinter(getCharacterNames()),
+      // don't auto select a single suggestion because use could be typing a
+      // new character name
+      completeSingle: false,
+    });
+  }
+}
+
+function makeTabStops(cm: CodeMirror.Editor): Array<TabStop> {
+  function selectToken(token: CodeMirror.Token, line: number): void {
+    cm.setSelection(
+      { line, ch: token.start },
+      { line, ch: token.end }
+    );
+  }
+
+  return [
+    {
+      name: 'left-sentinel',
+      activate() {
+        cm.operation(() => {
+          cm.execCommand('goLineLeft');
+        });
+      }
+    },
+    {
+      name: 'subject',
+      activate() {
+        const cursor = cm.getCursor();
+        const line = new LetteringLine(cm.getLineTokens(cursor.line));
+        const subject = line.getSubject();
+
+        if (subject) {
+          selectToken(subject, cursor.line);
+        }
+      }
+    },
+    {
+      name: 'modifier',
+      activate() {
+        const cursor = cm.getCursor();
+        const line = new LetteringLine(cm.getLineTokens(cursor.line));
+        const modifier = line.getModifier();
+
+        if (modifier) {
+          selectToken(modifier, cursor.line);
+        } else {
+          return 'skip';
+        }
+      }
+    },
+    {
+      name: 'content',
+      activate() {
+        const cursor = cm.getCursor();
+        const line = new LetteringLine(cm.getLineTokens(cursor.line));
+        const content = line.getContent();
+
+        if (content) {
+          selectToken(content, cursor.line);
+        }
+      }
+    },
+    {
+      name: 'right-sentinel',
+      activate() {
+        cm.operation(() => {
+          cm.execCommand('goLineRight');
+          cm.execCommand('newlineAndIndent');
+        });
+      }
+    }
+  ];
+}
+
+function makeHinter(characterNames: Array<string>) {
+  function hinter(cm: CodeMirror.Editor) {
+    const cursor = cm.getCursor();
+    const token = cm.getTokenAt(cursor);
+
+    const current = token.string.toLocaleUpperCase();
+
+    const suggestions = ['caption', 'sfx']
+      .concat(characterNames)
+      .map(suggestion => suggestion.toLocaleUpperCase())
+      .filter(suggestion => {
+        return current === SUBJECT_PLACEHOLDER
+          || suggestion.startsWith(current);
+      });
+
+    return {
+      list: suggestions,
+      from: {
+        line: cursor.line,
+        ch: token.start
+      },
+      to: {
+        line: cursor.line,
+        ch: token.end
+      }
+    };
+  }
+
+  // allow hint popup when text is selected
+  hinter.supportsSelection = true;
+
+  return hinter;
+}
+
+function makeKeyMap(next: () => void, prev: () => void, exit: () => void): CodeMirror.KeyMap {
+  return {
     Tab() {
       next();
     },
@@ -77,183 +246,4 @@ export function letteringSnippet(
       }
     }
   };
-
-  enter();
-  next();
-  showHint();
-
-  function enter() {
-    cm.addKeyMap(keyMap);
-    cm.on('cursorActivity', handleCursorActivity);
-    cm.addLineClass(lineNumber, 'text', LETTERING_SNIPPET_CLASS);
-
-    cm.replaceRange(`\t${SUBJECT_PLACEHOLDER}: content`, cm.getCursor());
-  }
-
-  function prev() {
-    move(-1);
-  }
-
-  function next() {
-    move(1);
-  }
-
-  function move(delta: number) {
-    stepIndex += delta;
-
-    const step = steps[stepIndex];
-
-    if (step) {
-      const result = step(cm, delta);
-
-      if (result === 'next') {
-        next();
-      } else if (result === 'prev') {
-        prev();
-      }
-    } else {
-      exit();
-    }
-  }
-
-  function exit() {
-    cm.removeLineClass(lineNumber, 'text', LETTERING_SNIPPET_CLASS);
-    cm.off('cursorActivity', handleCursorActivity);
-    cm.removeKeyMap(keyMap);
-  }
-
-  function handleCursorActivity(cm: CodeMirror.Editor) {
-    // Exit if it seems like user is trying to get out of snippet
-
-    // moved to different line
-    if (lineNumber !== cm.getCursor().line) {
-      exit();
-    }
-
-    // moved to start of line
-    if (cm.getCursor().ch === 0) {
-      exit();
-    }
-  }
-
-  function showHint() {
-    cm.showHint({
-      hint: makeHinter(getCharacterNames()),
-      // don't auto select a single suggestion because use could be typing a
-      // new character name
-      completeSingle: false,
-    });
-  }
-}
-
-function makeSteps() {
-  return [
-    function frontSentinel(cm: CodeMirror.Editor, stepDelta: number): any {
-      cm.operation(() => {
-        cm.execCommand('goLineLeft');
-      });
-    },
-    function subjectState(cm: CodeMirror.Editor, stepDelta: number): any {
-      const cursor = cm.getCursor();
-      const subject = cm.getLineTokens(cursor.line)
-        .find(token => {
-          return token.type?.includes(LETTERING_SUBJECT);
-        });
-
-      if (subject) {
-        cm.setSelection(
-          {
-            line: cursor.line,
-            ch: subject.start
-          },
-          {
-            line: cursor.line,
-            ch: subject.end
-          }
-        );
-      }
-    },
-    function modifierState(cm: CodeMirror.Editor, stepDelta: number): any {
-      const cursor = cm.getCursor();
-      const modifier = cm.getLineTokens(cursor.line)
-        .find(token => {
-          return token.type?.includes(LETTERING_MODIFIER);
-        });
-
-      if (modifier) {
-        cm.setSelection(
-          {
-            line: cursor.line,
-            ch: modifier.start
-          },
-          {
-            line: cursor.line,
-            ch: modifier.end
-          }
-        );
-      } else {
-        return stepDelta > 0 ? 'next' : 'prev';
-      }
-    },
-    function contentState(cm: CodeMirror.Editor, stepDelta: number): any {
-      const cursor = cm.getCursor();
-      const content = cm.getLineTokens(cursor.line)
-        .find(token => {
-          return token.type?.includes(LETTERING_CONTENT);
-        });
-
-      if (content) {
-        cm.setSelection(
-          {
-            line: cursor.line,
-            ch: content.start
-          },
-          {
-            line: cursor.line,
-            ch: content.end
-          }
-        );
-      }
-    },
-    function backSentinel(cm: CodeMirror.Editor, stepDelta: number): any {
-      cm.operation(() => {
-        cm.execCommand('goLineRight');
-        cm.execCommand('newlineAndIndent');
-      });
-    }
-  ];
-}
-
-function makeHinter(characterNames: Array<string>) {
-  function hinter(cm: CodeMirror.Editor) {
-    const cursor = cm.getCursor();
-    const token = cm.getTokenAt(cursor);
-
-    const current = token.string.toLocaleUpperCase();
-
-    const suggestions = ['caption', 'sfx']
-      .concat(characterNames)
-      .map(suggestion => suggestion.toLocaleUpperCase())
-      .filter(suggestion => {
-        return current === SUBJECT_PLACEHOLDER
-          || suggestion.startsWith(current);
-      });
-
-    return {
-      list: suggestions,
-      from: {
-        line: cursor.line,
-        ch: token.start
-      },
-      to: {
-        line: cursor.line,
-        ch: token.end
-      }
-    };
-  }
-
-  // allow hint popup when text is selected
-  hinter.supportsSelection = true;
-
-  return hinter;
 }
