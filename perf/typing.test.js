518837cf8105a2f0f19a97e97be1fae7e4f99819
Paul Salaets <psalaets@gmail.com>
2020-05-12 23:58:04 -0400

    minor test clean up

--- perf/typing.test.js
+++ perf/typing.test.js
@@ -5,7 +5,7 @@ describe('typing performance', () => {
   let page, browser, editor;
 
   beforeEach(async () => {
-    const headless = false;
+    const headless = true;
     browser = await playwright.chromium.launch({ headless });
     const context = await browser.newContext();
 
@@ -28,8 +28,6 @@ describe('typing performance', () => {
 
   test('typing into script', async () => {
     await editor.type('1234'.repeat(20), 5)
-
-    // await page.waitFor(1000);
 
     const stats = await editor.getStats('change-round-trip');
     console.log(stats);
