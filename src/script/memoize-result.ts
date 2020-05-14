6f613294a836ad5ed63f2b983dd24615f901c612
Paul Salaets <psalaets@gmail.com>
2020-05-13 23:30:04 -0400

    comment because I forgot what this is for

--- src/script/memoize-result.ts
+++ src/script/memoize-result.ts
@@ -1,3 +1,13 @@
+/**
+ * Memoize the result of a combiner function.
+ *
+ * This allows us to return a memoized value when inputs to the combiner have
+ * changed but the result is the same (as determined by custom logic) as the
+ * previous result.
+ *
+ * @param combiner - Combiner to wrap
+ * @param isEqual - Custom logic that defines what "the same" means
+ */
 export function memoizeResult<CombinerResultType, CombinerArgumentType extends any[]>(
   combiner: (...args: CombinerArgumentType) => CombinerResultType,
   isEqual: (lastResult: CombinerResultType | null, nextResult: CombinerResultType) => boolean
