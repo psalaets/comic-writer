a1df285a69de41c71c262e6f812992f7cd26f20a
Paul Salaets <psalaets@gmail.com>
2020-05-24 01:24:16 -0400

    rename so it stands out more in devtools

--- src/perf/wrap.ts
+++ src/perf/wrap.ts
@@ -1,7 +1,7 @@
 import { start, end } from './measure';
 
 /**
- * Wrap a function with timing.
+ * Wrap a function with perf timing.
  *
  * @param name Name to log in the timeline.
  * @param fn Function to wrap
@@ -10,7 +10,7 @@ export function wrap<T, A extends any[]>(
   name: string,
   fn: (...a: A) => T
 ): ((...a: A) => T) {
-  return function wrapper(...a: A) {
+  return function perfWrapper(...a: A) {
     start(name);
     const result = fn(...a);
     end(name);
